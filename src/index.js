f97e659c2375d25e23db94e850a6aa0ca48a0cd3
sichallez <sich.scidonnees@gmail.com>
2022-02-20 10:53:16 -0500

    Phase Four Load Data With Axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world from index.JS!!');
+// console.log('hello world from index.JS!!');
+
+const ul = document.querySelector('ul');
+
+const init = async() => {
+    const skills = (await axios.get('/api/technical_skills')).data;
+    const list = `
+        ${skills.map(skill => `
+            <li>${skill.name}: ${skill.category}</li>`).join('')}`;
+    ul.innerHTML = list;
+    // console.log(skills);
+};
+
+init();
